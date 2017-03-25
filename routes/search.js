c1938e81239eea1feea9490782c7aa1927d62207
Ikechi Michael <mykehell123@gmail.com>
2017-03-25 19:42:16 +0100

    feature: removing tweets by handle

--- routes/search.js
+++ routes/search.js
@@ -3,13 +3,17 @@ var router = express.Router();
 var Twitter = require('twitter-node-client').Twitter;
 var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 
+String.isNullOrEmpty = function (str) {
+    return str == null || str == '';
+}
+
 var getTwitterInstance = function () {
     return new Twitter({
-       "consumerKey": "JFIIqp4iWRViAonopq4Syq9L0",
-       "consumerSecret": "tNJ0Ln8LQLA63C0hSpbArUyyXW0RMY5ppqBuPoZ7mBIgqxpORb",
-       "accessToken": "845653889810661376-ppN8YdqJpDbKli6DxT4m8JVwQoCuC72",
-       "accessTokenSecret": "nLBvHDurH98kP2aNloptMpq4y8FYKCj0hJDHnW1nATtYN",
-       "callBackUrl": "https://customer-complaint.herokuapp.com/oauth/twitter"
+        "consumerKey": "JFIIqp4iWRViAonopq4Syq9L0",
+        "consumerSecret": "tNJ0Ln8LQLA63C0hSpbArUyyXW0RMY5ppqBuPoZ7mBIgqxpORb",
+        "accessToken": "845653889810661376-ppN8YdqJpDbKli6DxT4m8JVwQoCuC72",
+        "accessTokenSecret": "nLBvHDurH98kP2aNloptMpq4y8FYKCj0hJDHnW1nATtYN",
+        "callBackUrl": "https://customer-complaint.herokuapp.com/oauth/twitter"
     });
 }
 
@@ -21,11 +25,25 @@ var getToneAnalyzerInstance = function () {
     });
 }
 
-/* GET home page. */
-router.get('/:hashtag', function(req, res, next) {
+var getToneInfo = function (text) {
+    var tone_analyzer = getToneAnalyzerInstance();
+    return new Promise(function (resolve, reject) {
+        tone_analyzer.tone({ text: text }, function (err, tone) {
+            if (err) {
+                console.log(err);
+                reject('error', ex);
+            }
+            else {
+                resolve({ text: text, tone: tone })
+            }
+        })
+    });
+}
+
+var handleSearchRequestFn = function (req, res, next) {
     var twitter = getTwitterInstance();
     var tone_analyzer = getToneAnalyzerInstance();
-    
+
     var errorHandler = function (err) {
         console.log(err);
         res.render('search', { title: 'Sorry, an error occurred', results: err });
@@ -33,10 +51,23 @@ router.get('/:hashtag', function(req, res, next) {
 
     var successHandler = function (response) {
         var tweets = JSON.parse(response).statuses;
-        res.render('search', { title: 'Search Results', results: JSON.stringify(tweets.map(tweet => tweet.text)) });
+        if (!String.isNullOrEmpty(req.query.handle)) tweets = tweets.filter(tweet => ((tweet.user || {}).user.screen_name || '').toLowerCase() != req.query.handle.toLowerCase().replace(/\@/g, ''));
+        Promise.all(tweets.map(tweet => getToneInfo(tweet.text))).then(function (responses) {
+            return res.render('search', {
+                title: 'Search Results', results: JSON.stringify(responses.map((tone, i) => {
+                    var tweetTone = { tweet: tweets[i], tone: tone }
+                    return tweetTone;
+                }))
+            });
+        });
     }
 
-    twitter.getSearch({'q':req.params.hashtag, 'count': 10}, errorHandler, successHandler);
-});
+
+    twitter.getSearch({ 'q': req.params.hashtag || req.query.searchQuery, 'count': 10 }, errorHandler, successHandler);
+}
+
+/* GET home page. */
+router.get('/:hashtag', handleSearchRequestFn);
+router.get('/', handleSearchRequestFn);
 
 module.exports = router;
